bc931419f44a9cb0acab58a8610251115f897e7f
Davoud Tahrir <davidtahrir@gmail.com>
2024-12-14 11:14:24 +0100

    added some snippets

--- MyTestFile.py
+++ MyTestFile.py
@@ -1,5 +1,7 @@
 def SayHello(b):
-  a = input("Enter a cha: ")
+  a = input("Enter a charachter: ")
   print(b, a)
 
 SayHello("Hello")
+print("This is to add for Branch No2")
+print("To see if it is working...")
